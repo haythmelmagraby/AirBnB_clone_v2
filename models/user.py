0b0b4df9b092e365e6b254cb414ac4d60f51fd22
haythmelmagraby <haythmelmagraby@gmail.com>
2024-05-02 04:58:24 -0700

    users

--- models/user.py
+++ models/user.py
@@ -1,15 +1,18 @@
 #!/usr/bin/python3
 """This module defines a class User"""
 from models.base_model import BaseModel
-from models.base_model import Base
+from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column
 from sqlalchemy import String
 from sqlalchemy.orm import relationship
 
 
+Base = declarative_base()
+
+
 class User(BaseModel, Base):
     """This class defines a user by various attributes"""
-     __tablename__ = "users"
+    __tablename__ = 'users'
     email = Column(String(128), nullable=False)
     password = Column(String(128), nullable=False)
     first_name = Column(String(128))
