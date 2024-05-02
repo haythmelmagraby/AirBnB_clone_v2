09e59b72983b028cf027d49ef7708346b6020ef3
haythmelmagraby <haythmelmagraby@gmail.com>
2024-05-02 01:20:58 -0700

    4

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,8 +1,8 @@
---create db
+-- create db
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
---create user
+-- create user
 CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
---grant all privileges
+-- grant all privileges
 GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
 -- flush to access privilege
 FLUSH PRIVILEGES;
