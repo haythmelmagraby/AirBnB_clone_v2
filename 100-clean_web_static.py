9519d17dda1ffc9d10c2950ae580088ff3e2b75d
haythmelmagraby <haythmelmagraby@gmail.com>
2024-05-16 14:47:22 -0700

    100

---
+++ 100-clean_web_static.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Module deletes out-of-date archives"""
+
+import os
+from fabric.api import *
+
+
+def do_clean(n=0):
+    """do clean"""
+    if int(n) == 0:
+        n = 1
+
+    arch = sorted(os.listdir("versions"))
+    [arch.pop() for i in range(n)]
+    with lcd("versions"):
+        [local("rm ./{}".format(a)) for a in arch]
+
+    with cd("/data/web_static/releases"):
+        archs = run("ls -tr").split()
+        archs = [f for f in archs if "web_static_" in f]
+        [archs.pop() for i in range(n)]
+        [run("rm -rf ./{}".format(f)) for f in archs]
