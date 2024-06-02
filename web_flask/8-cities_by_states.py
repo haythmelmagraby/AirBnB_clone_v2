2e6111ac679a373c76ba775f69d9eabb6d6ec447
haythmelmagraby <haythmelmagraby@gmail.com>
2024-06-02 23:00:07 +0000

    9

---
+++ web_flask/8-cities_by_states.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" 9. Cities by states """"
+from models import storage
+from flask import Flask
+from flask import render_template
+app = Flask(__name__)
+
+@app.route("/cities_by_states", strict_slashes=False)
+def cities_by_states():
+    """ display cities """
+    states = storage.all("State")
+    return render_template("8-cities_by_states.html", states=states)
+
+@app.teardown_appcontext
+def teardown(exc):
+    """ remove session """
+    storage.close()
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
